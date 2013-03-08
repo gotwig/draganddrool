d8a66b34337693f6ac6ee837c2011609c4105de9
Ángel Guzmán Maeso <shakaran@gmail.com>
2013-03-08 16:28:45 +0100

    Implement add() method in Grid

--- lib/Grid.php
+++ lib/Grid.php
@@ -25,11 +25,26 @@ class Grid
 		foreach ($boxes as $box)
 		{
 			$box = array_map('intval', $box);
-			$sql = "UPDATE gridentries SET datarow='{$box['row']}' , datacolumn='{$box['col']}' WHERE id='{$box['id']}'";
+			$sql = "UPDATE gridentries SET datarow='{$box['row']}' , datacolumn='{$box['col']}' WHERE id='{$box['id']}'"; // @todo SQL-INYECTION
 			mysqli_query($link,$sql);
 		
 		}
 		
 		echo $sql;
+	}
+	
+	public static function add()
+	{
+		global $link;
+		
+		$t = mysqli_query($link, "INSERT INTO `grid`(`id`, `name`, `ownerid`, `lastchange`) 
+				VALUES ('', 'Put new Gridname here', '".$_SESSION['id']."', CURRENT_TIMESTAMP)"); // @todo SQL-INYECTION
+		
+		if ( !$t )
+		{
+			die('Error INSERT: ' . mysqli_error($link));
+		}
+		
+		echo mysqli_insert_id($link);
 	}
 }
--- php/add_grid.php
+++ php/add_grid.php
@@ -1,14 +1,7 @@
 <?php
 require_once 'connect_inc.php';
 require_once '../lib/Session.php';
+require_once '../lib/Grid.php';
 
 Session::init();
-
-$t = mysqli_query($link, "INSERT INTO `grid`(`id`, `name`, `ownerid`, `lastchange`) VALUES ('', 'Put new Gridname here', '".$_SESSION['id']."', CURRENT_TIMESTAMP)");
-
-if ( !$t ) 
-{
-	die('Error INSERT: ' . mysqli_error($link));
-}
-
-echo mysqli_insert_id($link);
+Grid::add();
