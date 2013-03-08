e36788eb25e8f3694ee40f4d494a769f99683fd1
Ángel Guzmán Maeso <shakaran@gmail.com>
2013-03-08 16:30:49 +0100

    Implement addUser() method in Grid

--- lib/Grid.php
+++ lib/Grid.php
@@ -47,4 +47,34 @@ class Grid
 		
 		echo mysqli_insert_id($link);
 	}
+	
+	public static function addUser()
+	{
+		global $link;
+		
+		$sql1 = "INSERT INTO `login`(`id`, `name`, `pwd`, `email`, `ban`, `lastusedgrid`) VALUES ('','".$_POST['new_username']."','".md5($_POST['new_password'])."','".$_POST['new_email']."', '', NULL)";
+		
+		
+		mysqli_query($link,$sql1);
+		
+		$sql2 =	"INSERT INTO `grid`(`id`, `name`, `ownerid`, `lastchange`) VALUES ('', 'Put new Gridname here', '".mysqli_insert_id($link)."', CURRENT_TIMESTAMP)";
+		
+		
+		$account_id = mysqli_insert_id($link);
+		
+		mysqli_query($link,$sql2);
+		
+		$sql3 = 'UPDATE login SET lastusedgrid="' . mysqli_insert_id($link) .'" WHERE id='.$account_id ;
+		
+		$_SESSION['gridid'] = mysqli_insert_id($link);
+		
+		mysqli_query($link,$sql3);
+		
+		
+		$_SESSION['id'] = $account_id;
+		$_SESSION['usr'] = $_POST['new_username'];
+		
+		
+		header("Location: ../project.php");
+	}
 }
--- php/add_user.php
+++ php/add_user.php
@@ -1,30 +1,8 @@
 <?php
 require_once 'connect_inc.php';
 require_once '../lib/Session.php';
+require_once '../lib/Grid.php';
 
 Session::init();
 
-$sql1 = "INSERT INTO `login`(`id`, `name`, `pwd`, `email`, `ban`, `lastusedgrid`) VALUES ('','".$_POST['new_username']."','".md5($_POST['new_password'])."','".$_POST['new_email']."', '', NULL)";
-
-
-mysqli_query($link,$sql1);
-
-$sql2 =	"INSERT INTO `grid`(`id`, `name`, `ownerid`, `lastchange`) VALUES ('', 'Put new Gridname here', '".mysqli_insert_id($link)."', CURRENT_TIMESTAMP)";
-
-
-$account_id = mysqli_insert_id($link);
-
-mysqli_query($link,$sql2);
-
-$sql3 = 'UPDATE login SET lastusedgrid="' . mysqli_insert_id($link) .'" WHERE id='.$account_id ;
-
-$_SESSION['gridid'] = mysqli_insert_id($link);
-
-mysqli_query($link,$sql3);
-
-
-$_SESSION['id'] = $account_id;
-$_SESSION['usr'] = $_POST['new_username'];
-
-
-header("Location: ../project.php");
+Grid::addUser();
