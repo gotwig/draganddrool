b6268990e7ae50fba950cf05fbb39416237d5a5a
Ángel Guzmán Maeso <shakaran@gmail.com>
2013-03-08 15:44:51 +0100

    Implement Login as a class

---
+++ lib/Login.php
@@ -0,0 +1,17 @@
+<?php
+class Login
+{
+	public static function isLogged()
+	{
+		return isset($_SESSION['id']);
+	}
+	
+	public static function checkLoginRedirect()
+	{
+		if(!self::isLogged())
+		{
+			header('Location: index.php');
+			exit();
+		}
+	}
+}
