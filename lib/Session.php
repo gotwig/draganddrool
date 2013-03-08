132b5da1df7051aa0acdf6b653be0927ea61c755
Ángel Guzmán Maeso <shakaran@gmail.com>
2013-03-08 15:56:46 +0100

    Implement destroy session method

--- lib/Session.php
+++ lib/Session.php
@@ -17,4 +17,17 @@ class Session
 		session_name(self::DEFAULT_SESSION_NAME);
 		session_set_cookie_params(self::COOKIE_EXPIRED_TIME);
 	}
+	
+	public static function destroy()
+	{
+		$_SESSION = array();
+		session_unset();
+		session_destroy();
+		header('Pragma: no-cache');
+		header('cache-Control: no-cache, must-revalidate'); // HTTP/1.1
+		header('Expires: Mon, 26 Jul 1997 05:00:00 GMT'); // Date in the past
+		//header('Location: /?logout=' . rand(0, 500000));
+		header('Location: index.php');
+		exit;
+	}
 }
