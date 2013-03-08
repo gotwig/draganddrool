4d954097b369532fd3e583ec5c1decddeaafd5c2
Ángel Guzmán Maeso <shakaran@gmail.com>
2013-03-08 16:25:54 +0100

    Use native intval function instead of ParseInt

--- php/save_position.php
+++ php/save_position.php
@@ -4,15 +4,10 @@ require_once 'connect_inc.php';
 $boxes = $_POST['data'];
 foreach ($boxes as $box)
 {
-	$box = array_map('parseInt', $box);
+	$box = array_map('intval', $box);
 	$sql = "UPDATE gridentries SET datarow='{$box['row']}' , datacolumn='{$box['col']}' WHERE id='{$box['id']}'";
 	mysqli_query($link,$sql);
 
-}
-
-function parseInt($v)
-{
-	return (int) $v;
 }
 
 echo $sql;
